32b6961ea6c17e6b452879a7ca2ac76681e6f585
DennisAndreev <denisandreev1611@gmail.com>
2019-07-21 16:18:08 +0300

    changeCards

--- GameCards/ViewController.swift
+++ GameCards/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var flipCount : Int = 0 {
+        didSet {
+             flipLabel.text = "Flips: \(flipCount)"
+        }
     }
+    var emojiChoice: Array<Any>  = ["😎","😘","😎","😘"]
+    
+    
+    @IBOutlet weak var flipLabel: UILabel!
 
+    @IBOutlet var cardButtons: [UIButton]!
+  
 
+    @IBAction func touchCard(_ sender: UIButton) {
+         flipCount += 1
+        if  let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoice[cardNumber] as! String, on: sender)
+        } else {
+            print("Error")
+        }
+        
+
+    }
+    
+  
+    
+    func flipCard(withEmoji emoji:String,on button : UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+    
 }
 
